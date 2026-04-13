950627620a36aa28866a15285cee97a54f051dee
fmoraless <franticzero@gmail.com>
2026-04-12 23:56:47 -0400

    feat: implement ProductGrid component and integrate it into Home page

--- src/app/(shop)/page.tsx
+++ src/app/(shop)/page.tsx
@@ -1,11 +1,15 @@
 import { Title } from "@/components/ui/title/Title";
-import { titleFont } from "@/config/fonts";
-import Image from "next/image";
+import { initialData } from "../seed";
+import { ProductGrid } from "@/components";
+
+const products = initialData.products;
 
 export default function Home() {
   return (
     <>
       <Title title="Tienda" subtitle="Todos los productos" className="mb-2" />
+
+      <ProductGrid products={products} />
     </>
   );
 }
